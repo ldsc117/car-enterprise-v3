1b9126ffe163740139ebc6bb6c59ae9648a1d3d6
Mihai-Stefan <mihaiste@gmail.com>
2021-06-04 01:30:58 +0300

    send emails to employees without car implemented

--- ClientApp/src/components/AdminPage.js
+++ ClientApp/src/components/AdminPage.js
@@ -1,6 +1,8 @@
 ﻿import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
+import {  NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 const AdminPage = () => {
@@ -28,6 +30,7 @@ const AdminPage = () => {
 
 
     return (
+        <>
         <div className='table-container'>
             <h4>List of Employees sort by Rank Points:</h4>
             <br></br>
@@ -41,6 +44,7 @@ const AdminPage = () => {
                         <th>Email</th>
                         <th>Phone</th>
                         <th>Points</th>
+                        <th>Has Car</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -55,17 +59,66 @@ const AdminPage = () => {
                                 <td>{employee.email}</td>
                                 <td>{employee.phoneNumber}</td>
                                 <td>{employee.rankPoints}</td>
+                                <td>{employee.assignedCar == null ? "N" : "Y"}</td>
                             </tr>)}
 
 
                 </tbody>
             </Table>
 
-            <h4>Input number of available cars:</h4>
-
-            {/*<SendEmails />*/}
 
         </div>
+
+
+
+
+            <div className='table-container'>
+                <br></br>
+            <h4>List of Employees without a car:</h4>
+            <br></br>
+
+            <Table className='table' striped bordered hover size="sm">
+                <thead>
+                    <tr>
+
+                        <th>ID</th>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Points</th>
+                        <th>Has Car</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {employees
+                        .sort((a, b) => (a.rankPoints < b.rankPoints)? 1 : -1)
+                            //.filter(a => a.registrationId == "1ca3dc48-3147-405e-9cb4-5c8535e5ee2d")
+                            .filter(a => a.assignedCar == null)
+
+                        .map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.firstName}  {employee.lastName}</td>
+                                <td>{employee.email}</td>
+                                <td>{employee.phoneNumber}</td>
+                                <td>{employee.rankPoints}</td>
+                                <td>{employee.AsignedCar == null? "N" : "Y"}</td>
+                            </tr>)}
+
+
+                </tbody>
+            </Table>
+
+
+                <a className="btn btn-success" href="https://localhost:44316/api/SendEmails">Send them Emails</a>
+
+            </div>
+
+            
+
+            </>
+
     )
 
 };
